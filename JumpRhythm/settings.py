b105df9d69f4f002b6837a5ee33481146280441f
wangxiaowen <tjwxw0904@163.com>
2018-06-08 23:59:37 +0800

    Added model User.

--- JumpRhythm/settings.py
+++ JumpRhythm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myaccount.apps.MyaccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
