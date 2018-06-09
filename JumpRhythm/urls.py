9f92a4295f818001e078c5ad2bed327584ebde34
wangxiaowen <tjwxw0904@163.com>
2018-06-09 11:13:26 +0800

    Changed default start page to myauth.index for dev...

--- JumpRhythm/urls.py
+++ JumpRhythm/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
+from myauth.views import index
 
 urlpatterns = [
+    url(r'^$', index),
     path('jumprhythm/', include('myauth.urls')),
     path('admin/', admin.site.urls),
 ]
