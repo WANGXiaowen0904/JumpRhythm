c8a0e410da6b02d49c296a7d1c46019a3de2ee8e
wangxiaowen <tjwxw0904@163.com>
2018-06-17 10:34:24 +0800

    Added model Score.

--- rhythm/models.py
+++ rhythm/models.py
@@ -19,3 +19,12 @@ class Fragment(models.Model):
 
     def __str__(self):
         return self.user.username + ': ' + self.upload.name
+
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return ','.join([self.user.username, str(self.score), str(self.created_at)])
